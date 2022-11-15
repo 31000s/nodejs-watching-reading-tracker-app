f295411c0642e20e754a02cae666660da547dfe5
31000s <burakyabgu@gmail.com>
2022-11-16 01:36:53 +0200

    MongoDB'nin app.js'e dâhil edilmesi.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+
+mongoose.connect('localhost', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 const app = express();
 
@@ -10,21 +16,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
 app.get('/books', (req, res) => {
-    res.render('books');
+  res.render('books');
 });
 
 app.get('/movies', (req, res) => {
-    res.render('movies');
+  res.render('movies');
 });
 
 app.get('/series', (req, res) => {
-    res.render('series');
+  res.render('series');
 });
 
-app.listen(3000, () =>{
-    console.log("Sunucu Başlatıldı");
+app.listen(3000, () => {
+  console.log('Sunucu Başlatıldı');
 });
