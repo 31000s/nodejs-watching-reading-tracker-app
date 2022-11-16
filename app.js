fb181bc16d5e8cab548fe4c1160e2502655f929c
31000s <burakyabgu@gmail.com>
2022-11-16 03:04:11 +0200

    Dizi, Film ve Kitap Ekleme Yerinin Tamamlanması

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 
-mongoose.connect('localhost', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+const Movie = require('./models/Movie');
+const Serie = require('./models/Series');
+const Book = require('./models/Book');
+
+mongoose.connect('', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const app = express();
@@ -15,12 +19,15 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/books', (req, res) => {
-  res.render('books');
+app.get('/', async (req, res) => {
+  const Movies = await Movie.find({});
+  const Series = await Serie.find({});
+  const Books = await Book.find({});
+  res.render('index', {
+    Movies,
+    Series,
+    Books,
+  });
 });
 
 app.get('/movies', (req, res) => {
@@ -31,9 +38,25 @@ app.get('/series', (req, res) => {
   res.render('series');
 });
 
-app.get('/add', (req,res) => {
-    res.render('add');
-})
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+//ADD
+app.post('/movies', async (req, res) => {
+  await Movie.create(req.body);
+  res.redirect('/');
+});
+
+app.post('/series', async (req, res) => {
+  await Serie.create(req.body);
+  res.redirect('/');
+});
+
+app.post('/books', async (req, res) => {
+  await Book.create(req.body);
+  res.redirect('/');
+});
 
 app.listen(3000, () => {
   console.log('Sunucu Başlatıldı');
