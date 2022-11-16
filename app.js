526ee5df24ce3a9e5fbd0b445b60339939d04a63
31000s <burakyabgu@gmail.com>
2022-11-16 03:39:25 +0200

    Dizi, Film ve Kitap Yerlerinin Tamamlanması.

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 
-const Movie = require('./models/Movie');
-const Serie = require('./models/Series');
-const Book = require('./models/Book');
+const Movie = require('./models/Movie')
+const Serie = require('./models/Series')
+const Book = require('./models/Book')
 
 mongoose.connect('', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 const app = express();
@@ -20,42 +20,48 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  const Movies = await Movie.find({});
-  const Series = await Serie.find({});
-  const Books = await Book.find({});
-  res.render('index', {
-    Movies,
-    Series,
-    Books,
+    res.render('index');
+});
+
+app.get('/movie', async (req, res) => {
+    const Movies = await Movie.find({})
+    res.render('movie', {
+        Movies
+    });
+});
+
+app.get('/serie', async (req, res) => {
+    const Series = await Serie.find({})
+    res.render('serie', {
+        Series
+    });
   });
-});
 
-app.get('/movies', (req, res) => {
-  res.render('movies');
-});
+app.get('/book', async (req, res) => {
+    const Books = await Book.find({})
+    res.render('book', {
+        Books
+    });
+  });
 
-app.get('/series', (req, res) => {
-  res.render('series');
-});
-
-app.get('/add', (req, res) => {
-  res.render('add');
+app.get('/add', (req,res) => {
+    res.render('add');
 });
 
 //ADD
-app.post('/movies', async (req, res) => {
-  await Movie.create(req.body);
-  res.redirect('/');
+app.post('/movies', async (req,res) =>{
+    await Movie.create(req.body);
+    res.redirect('/')
 });
 
-app.post('/series', async (req, res) => {
-  await Serie.create(req.body);
-  res.redirect('/');
+app.post('/series', async (req,res) =>{
+    await Serie.create(req.body);
+    res.redirect('/')
 });
 
-app.post('/books', async (req, res) => {
-  await Book.create(req.body);
-  res.redirect('/');
+app.post('/books', async (req,res) =>{
+    await Book.create(req.body);
+    res.redirect('/')
 });
 
 app.listen(3000, () => {
